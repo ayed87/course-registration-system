7ea752e4b3fbb9c475ac9e8f7c05b0498d5c1383
ayed87 <gake12234@gmail.com>
2022-11-22 22:33:56 +0300

    add java project

--- ics108-project/src/App.java
+++ ics108-project/src/App.java
@@ -1,5 +1,20 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class App extends Application {
+  @Override // Override the start method in the Application class
+  public void start(Stage primaryStage) {
+    // Create a button and place it in the scene
+    Button btOK = new Button("OK");
+    Scene scene = new Scene(btOK, 500, 250);
+    primaryStage.setTitle("MyJavaFX"); // Set the stage title
+    primaryStage.setScene(scene); // Place the scene in the stage
+    primaryStage.show(); // Display the stage
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
