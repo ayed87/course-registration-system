b5f95bd23db97ac1d57e7ac92e28255e977e69d8
Ziyad <ziyad.awadh.alshamrani@gmail.com>
2022-11-28 01:00:50 +0300

    Data classes zyad

--- ics108-project/src/Data/Section.java
+++ ics108-project/src/Data/Section.java
@@ -1,6 +1,15 @@
 package Data;
 
-public class Section extends Course{
+public class Section extends Course {
+
+    private Course course; 
+    private String day; 
+    private String time; 
+    private String location; 
+    private String status; 
+    private String waitlist;
+    private String type;
+    private String crn; 
 
     
     
